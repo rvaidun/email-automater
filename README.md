b9fea914ec19754b0981e55f4c5d7035e4c26fa7
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2025-03-05 23:11:51 -0800

    batman

---
+++ README.md
@@ -0,0 +1,60 @@
+# Automate Recruiter Emails
+This is a simple python script that automates the process of sending emails to recruiters. It uses the gmail API to draft emails to recruiters. Currently I use it in conjunction with Streak CRM to manage my job search. I write the draft using this script and then later schedule the email to be sent later manually using. See a demo [here](https://youtu.be/Ef5i8DboJP4).
+
+# Installation
+1. Clone the repository
+2. Create a virtual environment `python3 -m venv venv`
+3. Activate the virtual environment `source venv/bin/activate`
+4. Install the requirements `pip install -r requirements.txt`
+5. Follow the instructions [here](https://developers.google.com/gmail/api/quickstart/python) to enable the Gmail API and download the `credentials.json` file. Save the file in the root directory of the repository.
+# Usage
+Directory structure
+```
+.
+├── README.md
+├── automate_emails.py
+├── credentials.json
+├── email_template.txt
+├── requirements.txt
+├── resume.pdf
+└── token.json
+```
+
+Script usage
+```
+(venv) ➜  automate_emails ./automate_emails.py -h 
+usage: automate_emails.py [-h] filename recruiter_name recruiter_company recruiter_email
+
+Automates sending emails to recruiters
+
+positional arguments:
+  filename           The name of the file to process
+  recruiter_name     The full name of the recruiter
+  recruiter_company  The company name of the recruiter
+  recruiter_email    The email address of the recruiter
+
+options:
+  -h, --help         show this help message and exit
+```
+
+- `filename`: The name of the file to process. The file should use [Python's templating syntax](https://docs.python.org/3.3/tutorial/stdlib2.html#templating) to have the following variables: `recruiter_name`, `$recruiter_company`. The script will replace these variables with the values provided in the command line arguments. For example, the file could look like this:
+```
+Dear $recruiter_name,
+
+I am interested in the position at $recruiter_company.
+```
+
+The following environment variables should be set:
+
+```
+EMAIL_SUBJECT=I would like to work at $recruiter_company
+ATTACHMENT_PATH=resume.pdf
+ATTACHMENT_NAME=FirstName_LastName_Resume.pdf
+```
+
+Notice the subject can also use the templating syntax.
+
+6. Run the script `./automate_emails.py filename recruiter_name recruiter_company recruiter_email`
+
+# Future
+I created this just to help me with my job search. I'm not really planning on adding any new features. However, if you have any suggestions or find any bugs feel free to open an issue or a pull request. The only improvement I can think of is to add a feature to schedule the emails to be sent later and adding more template variables.
---
+++ automate_emails.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import argparse
+from string import Template
+import base64
+from email.mime.base import MIMEBase
+from email import encoders
+from email.message import EmailMessage
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://mail.google.com/"]
+
+
+def get_gmail_service():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    service = build("gmail", "v1", credentials=creds)
+    return service
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Automates sending emails to recruiters")
+    parser.add_argument("filename", type=str,
+                        help="The name of the file to process")
+    parser.add_argument("recruiter_name", type=str,
+                        help="The full name of the recruiter")
+    parser.add_argument("recruiter_company", type=str,
+                        help="The company name of the recruiter")
+    parser.add_argument("recruiter_email", type=str,
+                        help="The email address of the recruiter")
+    return parser.parse_args()
+
+
+def process_file(filename, **kwargs):
+    with open(filename, 'r') as file:
+        file_contents = file.read()
+
+    template = Template(file_contents)
+
+    substituted_contents = template.substitute(**kwargs)
+
+    return substituted_contents
+
+
+def process_subject(s, **kwargs):
+    template = Template(s)
+    return template.substitute(**kwargs)
+
+
+def save_draft(service, user_id, message_body, to_address=None, subject=None, attachment=None):
+    base64_str = base64.urlsafe_b64encode(message_body.encode()).decode()
+    message = EmailMessage()
+
+    message.set_content(message_body)
+    if attachment:
+        with open(attachment, 'rb') as file:
+            content = file.read()
+            message.add_attachment(content, maintype='application',
+                                   subtype='octet-stream', filename=os.getenv("ATTACHMENT_NAME", attachment))
+
+    message['To'] = to_address
+    message['Subject'] = subject
+
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    parts = []
+    try:
+        draft_message = {'message': {'raw': encoded_message}}
+        draft = service.users().drafts().create(
+            userId=user_id, body=draft_message).execute()
+        print(f"Draft id: {draft['id']}\nDraft message: {draft['message']}")
+        return draft
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return None
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    service = get_gmail_service()
+    recruiter = args.recruiter_name
+    recruiter_name = args.recruiter_name
+    recruiter_company = args.recruiter_company
+    recruiter_email = args.recruiter_email
+    filename = args.filename
+    subject = os.getenv("EMAIL_SUBJECT")
+    attachment = os.getenv("ATTACHMENT_PATH")
+    email_contents = process_file(
+        filename, recruiter_name=recruiter_name, recruiter_company=recruiter_company)
+    subject = process_subject(subject, recruiter_company=recruiter_company)
+    print(subject)
+    print(email_contents)
+    save_draft(service, 'me', email_contents, to_address=recruiter_email,
+               subject=subject, attachment=attachment)
