bb6458a15082aa6aebb9d1ae808cc7d7b3ce2d79
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2025-03-20 13:59:39 -0700

    modularize gmail

--- automate_emails.py
+++ automate_emails.py
@@ -1,118 +1,123 @@
 #!/usr/bin/env python
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+"""Automates sending emails to recruiters."""
 import argparse
-from string import Template
-import base64
+import json
+import logging
+import os.path
+import sys
 from email.message import EmailMessage
+from pathlib import Path
+from string import Template
 
 from dotenv import load_dotenv
+
+from utils.gmail import GmailAPI
+
 load_dotenv()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://mail.google.com/"]
 
 
-def get_gmail_service():
-    """
-    Return a gmail service object
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-    service = build("gmail", "v1", credentials=creds)
-    return service
-
-
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Automates sending emails to recruiters")
-    parser.add_argument("recruiter_company", type=str,
-                        help="The company name of the recruiter")
-    parser.add_argument("recruiter_name", type=str,
-                        help="The full name of the recruiter")
+        description="Automates sending emails to recruiters"
+    )
+    parser.add_argument(
+        "recruiter_company", type=str, help="The company name of the recruiter"
+    )
+    parser.add_argument(
+        "recruiter_name", type=str, help="The full name of the recruiter"
+    )
 
-    parser.add_argument("recruiter_email", type=str,
-                        help="The email address of the recruiter")
-    parser.add_argument("--template_path", type=str,
-                        help="The path to the attachment file", default=os.getenv("MESSAGE_BODY_PATH"), nargs='?')
+    parser.add_argument(
+        "recruiter_email", type=str, help="The email address of the recruiter"
+    )
+    parser.add_argument(
+        "--template_path",
+        type=str,
+        help="The path to the attachment file",
+        default=os.getenv("MESSAGE_BODY_PATH"),
+        nargs="?",
+    )
     return parser.parse_args()
 
 
-def process_file(filename, **kwargs):
-    with open(filename, 'r') as file:
-        file_contents = file.read()
+def process_string(s: str, **kwargs: dict) -> str:
+    """
+    Process a file and substitute placeholders with values.
 
-    template = Template(file_contents)
+    filename: The name of the file to process.
+    **kwargs: The key-value pairs to substitute in the file.
+    For example, if the file contains the placeholder ${name},
+    you can pass name='John' to substitute it with 'John'.
 
-    substituted_contents = template.substitute(**kwargs)
-
-    return substituted_contents
-
-
-def process_subject(s, **kwargs):
+    Returns the processed file contents as a string.
+    """
     template = Template(s)
     return template.substitute(**kwargs)
 
 
-def save_draft(service, user_id, message_body, to_address=None, subject=None, attachment=None):
+def create_email_message(message_body: str, to_address: str, subject: str,
+                         attachment: bytes | None = None,
+                         attachment_name: str | None = None) -> EmailMessage:
+    """
+    Create an email message.
+
+    message_body: The body of the email message.
+    to_address: The email address of the recipient.
+    subject: The subject of the email message.
+    attachment: The path to the attachment file, if any.
+
+    Returns an EmailMessage object.
+    """
     message = EmailMessage()
 
-    message.set_content(message_body, subtype='html')
+    message.set_content(message_body, subtype="html")
     if attachment:
-        with open(attachment, 'rb') as file:
-            content = file.read()
-            message.add_attachment(content, maintype='application',
-                                   subtype='octet-stream', filename=os.getenv("ATTACHMENT_NAME", attachment))
+        if attachment_name:
+            message.add_attachment(attachment, maintype="application",
+                                   subtype="octet-stream", filename=attachment_name)
+        else:
+            logger.error("Attachment name not provided, skipping attachment")
 
-    message['To'] = to_address
-    message['Subject'] = subject
+    message["To"] = to_address
+    message["Subject"] = subject
 
-    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    try:
-        draft_message = {'message': {'raw': encoded_message}}
-        draft = service.users().drafts().create(
-            userId=user_id, body=draft_message).execute()
-        print(f"Draft id: {draft['id']}\nDraft message: {draft['message']}")
-        return draft
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return None
+    return message
 
 
 if __name__ == "__main__":
     args = parse_args()
-    service = get_gmail_service()
+    gmail_api = GmailAPI()
     recruiter = args.recruiter_name
     recruiter_name = args.recruiter_name
     recruiter_company = args.recruiter_company
     recruiter_email = args.recruiter_email
-    filename = args.template_path
     subject = os.getenv("EMAIL_SUBJECT")
     attachment = os.getenv("ATTACHMENT_PATH")
-    email_contents = process_file(
-        filename, recruiter_name=recruiter_name, recruiter_company=recruiter_company)
-    subject = process_subject(subject, recruiter_company=recruiter_company)
-    save_draft(service, 'me', email_contents, to_address=recruiter_email,
-               subject=subject, attachment=attachment)
+    if Path.exists("token.json"):
+        with Path.open("token.json", "rw") as file:
+            token = file.read()
+            token_json = json.loads(token)
+            creds = gmail_api.login(token_json)
+            file.write(creds.to_json())
+    else:
+        logger.error("No token.json file found")
+        sys.exit(1)
+    with Path.open(args.template_path, "r") as file:
+        template = file.read()
+
+    email_contents = process_string(
+        template, recruiter_name=recruiter_name, recruiter_company=recruiter_company
+    )
+    subject = process_string(subject, recruiter_company=recruiter_company)
+    email_message = create_email_message(
+        email_contents, recruiter_email, subject, attachment=attachment
+    )
+    gmail_api.save_draft(email_message)
---
+++ utils/gmail.py
@@ -0,0 +1,78 @@
+import base64
+import logging
+from email.message import EmailMessage
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+logger = logging.getLogger(__name__)
+SCOPES = ["https://mail.google.com/"]
+
+
+class GmailAPI:
+    """A class to interact with the Gmail API."""
+
+    def __init__(self, token: dict) -> None:
+        """Initialize the GmailAPI object."""
+
+    def login(self, token: dict | None = None) -> Credentials:
+        """
+        Log in to the Gmail API and sets self.service to the authenticated service.
+
+        Args:
+            token (dict): The token information to use for authentication.
+            Defaults to None.
+            If None, the user will be prompted to log in.
+
+        Returns:
+            Credentials: The credentials to use for authentication for next run.
+
+
+        """
+        creds = None
+        if token:
+            creds = Credentials.from_authorized_user_info(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+        self.service = build("gmail", "v1", credentials=creds)
+        return creds
+
+    def save_draft(
+        self,
+        message: EmailMessage,
+    ) -> bool:
+        """
+        Save a draft message in Gmail.
+
+        Args:
+            message (EmailMessage): The message to save as a draft.
+
+        Returns:
+            bool: True if the draft was saved successfully, False otherwise.
+
+        """
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        try:
+            draft_message = {"message": {"raw": encoded_message}}
+            draft = (
+                self.service.users()
+                .drafts()
+                .create(userId="me", body=draft_message)
+                .execute()
+            )
+
+            logger.info("Draft id: %s\nDraft message: %s",
+                        draft["id"], draft["message"])
+        except HttpError:
+            logger.exception("An error occurred saving the draft")
+            return False
+        return True
